b0b6b63c9a0bc98569cb1db30f0ddb455fb05e35
Jason Ozias <jason.g.ozias@gmail.com>
2023-11-05 16:15:46 -0500

    initial commit

---
+++ build.rs
@@ -0,0 +1,10 @@
+use anyhow::Result;
+use vergen::EmitBuilder;
+
+pub fn main() -> Result<()> {
+    EmitBuilder::builder()
+        .all_build()
+        .all_cargo()
+        .all_git()
+        .emit()
+}
