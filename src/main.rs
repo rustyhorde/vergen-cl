403f454e5db09da427a4d66cb9137fbac0992a73
Jason Ozias <jason.g.ozias@gmail.com>
2023-11-05 16:44:04 -0500

    added vergen-pretty

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use vergen_pretty::{vergen_pretty_env, PrettyBuilder};
+
+fn main() -> Result<()> {
+    let mut stdout = vec![];
+    PrettyBuilder::default()
+        .env(vergen_pretty_env!())
+        .build()?
+        .display(&mut stdout)?;
+    println!("{}", String::from_utf8_lossy(&stdout));
+    Ok(())
 }
