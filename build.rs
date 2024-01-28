ca4c95fab24fb5b6eb387f672557e4c42abc05fb
Jason Ozias <jason.g.ozias@gmail.com>
2024-01-28 13:34:04 -0500

    Removed some tweaks

--- build.rs
+++ build.rs
@@ -1,13 +1,11 @@
 use anyhow::Result;
-use vergen::{DependencyKind, EmitBuilder};
+use vergen::EmitBuilder;
 
 pub fn main() -> Result<()> {
     EmitBuilder::builder()
         .all_build()
         .all_cargo()
-        .cargo_dependencies_dep_kind_filter(Some(DependencyKind::Normal))
         .all_git()
-        .git_describe(true, true, None)
         .all_rustc()
         .all_sysinfo()
         .emit()
