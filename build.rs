c9f272e3b8ba99678e8312ead2458d7f2eea24da
Jason Ozias <jason.g.ozias@gmail.com>
2024-01-30 09:27:46 -0500

    updated to use latest vergen-gix stuff

--- build.rs
+++ build.rs
@@ -1,12 +1,48 @@
+use std::collections::BTreeMap;
+
 use anyhow::Result;
-use vergen::EmitBuilder;
+use vergen_gix::{
+    AddCustomEntries, BuildBuilder, CargoBuilder, CargoRerunIfChanged, CargoWarning, DefaultConfig,
+    DependencyKind, Emitter, GixBuilder, RustcBuilder, SysinfoBuilder,
+};
 
 pub fn main() -> Result<()> {
-    EmitBuilder::builder()
-        .all_build()
-        .all_cargo()
-        .all_git()
-        .all_rustc()
-        .all_sysinfo()
+    let mut cargo = CargoBuilder::all_cargo()?;
+    _ = cargo.set_dep_kind_filter(Some(DependencyKind::Normal));
+
+    Emitter::default()
+        .add_instructions(&BuildBuilder::all_build()?)?
+        .add_instructions(&cargo)?
+        .add_instructions(&GixBuilder::all_git()?)?
+        .add_instructions(&RustcBuilder::all_rustc()?)?
+        .add_instructions(&SysinfoBuilder::all_sysinfo()?)?
+        .add_custom_instructions(&Custom::default())?
         .emit()
+}
+
+#[derive(Default)]
+struct Custom {}
+
+impl AddCustomEntries<&str, &str> for Custom {
+    fn add_calculated_entries(
+        &self,
+        _idempotent: bool,
+        cargo_rustc_env_map: &mut BTreeMap<&str, &str>,
+        _cargo_rerun_if_changed: &mut CargoRerunIfChanged,
+        cargo_warning: &mut CargoWarning,
+    ) -> Result<()> {
+        cargo_rustc_env_map.insert("vergen-cl", "custom_instruction");
+        cargo_warning.push("custom instruction generated".to_string());
+        Ok(())
+    }
+
+    fn add_default_entries(
+        &self,
+        _config: &DefaultConfig,
+        _cargo_rustc_env_map: &mut BTreeMap<&str, &str>,
+        _cargo_rerun_if_changed: &mut CargoRerunIfChanged,
+        _cargo_warning: &mut CargoWarning,
+    ) -> Result<()> {
+        Ok(())
+    }
 }
